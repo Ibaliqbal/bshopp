20419fb5a174a8bfd984eca6d8b0f31b61ccee54
Iqbal Muthahhary <iqbalmuhahhary@gmail.com>
2024-04-21 12:43:32 +0700

    fix : error yang tidak diketahui

--- src/context/cart/cart.context.tsx
+++ src/context/cart/cart.context.tsx
@@ -42,29 +42,24 @@ export const CartProvider = ({
     }
   }
 
-  // React.useEffect(() => {
-  //   if (findUser) {
-  //     detail();
-  //   }
-  // }, [users]);
-
   React.useEffect(() => {
-    const unsub = onSnapshot(
-      doc(firestore, "users", findUser?.id ?? ""),
-      (snaphsot) => {
-        if (snaphsot.exists()) {
-          const data = {
-            ...snaphsot.data(),
-            id: snaphsot.id,
-          } as User;
-
-          setCart(data.cart);
-        }
-      }
-    );
-
-    return () => unsub();
+    if (findUser) {
+      detail();
+    }
   }, [users]);
+
+  // React.useEffect(() => {
+  //   const unsub = onSnapshot(
+  //     doc(firestore, "users", findUser?.id ?? ""),
+  //     (snaphsot) => {
+  //       if (snaphsot.exists()) {
+  //         setCart(snaphsot.data().cart);
+  //       }
+  //     }
+  //   );
+
+  //   return () => unsub();
+  // }, [users]);
 
   const handleAdd = async (data: Cart) => {
     if (!findUser?.id) return;
@@ -87,10 +82,10 @@ export const CartProvider = ({
         });
         if (res.status === 200) {
           toast.success(res.data.message);
-          // detail();
+          detail();
         } else {
           toast.error(res.data.message);
-          // detail();
+          detail();
         }
       } else {
         const res = await userService.updateUser(findUser?.id ?? "", {
@@ -98,10 +93,10 @@ export const CartProvider = ({
         });
         if (res.status === 200) {
           toast.success(res.data.message);
-          // detail();
+          detail();
         } else {
           toast.error(res.data.message);
-          // detail();
+          detail();
         }
       }
     }
@@ -117,10 +112,10 @@ export const CartProvider = ({
     });
     if (res.status === 200) {
       toast.success(res.data.message);
-      // detail();
+      detail();
     } else {
       toast.error(res.data.message);
-      // detail();
+      detail();
     }
   };
 
@@ -133,10 +128,10 @@ export const CartProvider = ({
     });
     if (res.status === 200) {
       toast.success(res.data.message);
-      // detail();
+      detail();
     } else {
       toast.error(res.data.message);
-      // detail();
+      detail();
     }
   };
 
@@ -161,10 +156,10 @@ export const CartProvider = ({
     });
     if (res.status === 200) {
       toast.success(res.data.message);
-      // detail();
+      detail();
     } else {
       toast.error(res.data.message);
-      // detail();
+      detail();
     }
   };
 
@@ -197,10 +192,10 @@ export const CartProvider = ({
     });
     if (res.status === 200) {
       toast.success(res.data.message);
-      // detail();
+      detail();
     } else {
       toast.error(res.data.message);
-      // detail();
+      detail();
     }
   };
   return (
